5cab9b9414bb862afb0883a6edc2b4fe699708de
emxia18 <emxia18@gmail.com>
2022-01-22 12:02:22 -0800

    new subsystem for ejector

---
+++ src/main/java/com/team1678/frc2022/subsystems/Ejector.java
@@ -0,0 +1,122 @@
+package com.team1678.frc2022.subsystems;
+
+import java.time.Period;
+
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
+import com.team1678.frc2022.ColorSensor;
+import com.team1678.frc2022.Constants;
+import com.team1678.frc2022.loops.ILooper;
+import com.team1678.frc2022.loops.Loop;
+import com.team1678.lib.drivers.REVColorSensorV3Wrapper;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
+
+public class Ejector extends Subsystem{
+    private static Ejector mInstance;
+
+    public PeriodicIO mPeriodicIO = new PeriodicIO();
+    private REVColorSensorV3Wrapper mColorSensor;
+    
+    public ColorSensor mColorSensorThread = new ColorSensor(mColorSensor);
+    private final ColorMatch mColorMatcher = new ColorMatch();
+
+    public ColorChoices mAllianceColor;
+    public ColorChoices mMathcedColor;
+
+    public enum ColorChoices {
+        RED, BLUE, NONE
+    }
+    
+    public static synchronized Ejector getInstance() {
+        if (mInstance == null) {
+            mInstance = new Ejector();
+        }
+        return mInstance;
+    }
+
+    private Ejector() {
+        mAllianceColor = Constants.isRedAlliance ? ColorChoices.RED : ColorChoices.BLUE;
+        mMathcedColor = ColorChoices.NONE;
+        mColorSensor = new REVColorSensorV3Wrapper(I2C.Port.kOnboard);
+
+        mColorMatcher.addColorMatch(Constants.EjectorConstants.kRedBallColor);
+        mColorMatcher.addColorMatch(Constants.EjectorConstants.kBlueBallColor);
+    }
+
+    public boolean getEject() {
+        return mPeriodicIO.eject;
+    }
+
+    @Override
+   public void registerEnabledLoops(ILooper enabledLooper) {
+       enabledLooper.register(new Loop() {
+           @Override
+           public void onStart(double timestamp) {
+                mColorSensorThread.start();
+                mPeriodicIO.eject = false;
+           }
+
+           @Override
+           public void onLoop(double timestamp) {
+           }
+
+           @Override
+           public void onStop(double timestamp) {
+                mColorSensor.stop();
+           }
+       });
+   }
+
+   @Override
+   public synchronized void readPeriodicInputs() {
+
+        if (mColorSensorThread.getColorSensorData() != null) {
+            mPeriodicIO.rawColor = mColorSensorThread.getColorSensorData().color;
+            mPeriodicIO.distance = mColorSensorThread.getColorSensorData().distance; 
+            mPeriodicIO.matchedColor = mColorMatcher.matchClosestColor(mPeriodicIO.rawColor).color; 
+
+            if (mPeriodicIO.distance < Constants.EjectorConstants.kColorSensorThreshold) {
+                mPeriodicIO.eject = false;
+                mMathcedColor = ColorChoices.NONE;
+            } else {
+                if (mPeriodicIO.matchedColor == Constants.EjectorConstants.kRedBallColor) {
+                mMathcedColor = ColorChoices.RED;
+                } else if (mPeriodicIO.matchedColor == Constants.EjectorConstants.kBlueBallColor) {
+                mMathcedColor = ColorChoices.BLUE;
+                }
+                mPeriodicIO.eject = mMathcedColor == mAllianceColor;
+            }
+        }
+
+   }
+
+   @Override
+   public void writePeriodicOutputs() {
+
+   }
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean checkSystem() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public static class PeriodicIO {
+    
+        public boolean eject;
+        public Color rawColor;
+        public int distance;
+        public Color matchedColor;
+
+    }
+    
+}
