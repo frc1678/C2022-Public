06f5aef540dc2c15acef3116422c4a9c286e5520
emxia18 <emxia18@gmail.com>
2022-01-22 12:02:46 -0800

    allow for color sensor to be a parameter

--- src/main/java/com/team1678/frc2022/ColorSensor.java
+++ src/main/java/com/team1678/frc2022/ColorSensor.java
@@ -3,12 +3,14 @@ package com.team1678.frc2022;
 import com.team1678.lib.drivers.REVColorSensorV3Wrapper;
 import com.team1678.lib.drivers.REVColorSensorV3Wrapper.ColorSensorData;
 
-import edu.wpi.first.wpilibj.util.Color;
-
 public class ColorSensor extends Thread{
 
     private REVColorSensorV3Wrapper mColorSensor;
     private ColorSensorData mRawColorData;
+
+    public ColorSensor(REVColorSensorV3Wrapper colorSensor) {
+        mColorSensor = colorSensor;
+    }
 
     public void run() {
         try {
