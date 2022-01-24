5485b46c72ff3784f1ed2c51dd5501dd38a38a71
Olivia Sagayaradj <oliviasagayaradj@Olivias-MacBook-Pro.local>
2022-01-23 16:16:48 -0800

    beta changes

--- src/main/java/com/team1678/frc2022/Ports.java
+++ src/main/java/com/team1678/frc2022/Ports.java
@@ -12,7 +12,7 @@ public class Ports {
         3 4
     */
 
-    public static final int FL_DRIVE = 0; 
+    public static final int FL_DRIVE = 0;
     public static final int FL_ROTATION = 1;
     public static final int FL_CANCODER = 0; 
 
