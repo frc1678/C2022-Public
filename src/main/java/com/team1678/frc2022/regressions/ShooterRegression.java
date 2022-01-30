40bb144fdafb55d788dd6d47feae600671337752
apurvagyan <apurvagyan@gmail.com>
2022-01-30 15:57:45 -0800

    add Jama package files and include polynomial regression calculator

---
+++ src/main/java/com/team1678/frc2022/regressions/ShooterRegression.java
@@ -0,0 +1,82 @@
+package com.team1678.frc2022.regressions;
+
+import com.team254.lib.util.PolynomialRegression;
+import com.team254.lib.util.InterpolatingTreeMap;
+import com.team254.lib.util.InterpolatingDouble;
+
+
+public class ShooterRegression {
+    public static final double kHoodPaddingDegrees = 2;
+    public static final double kShooterPaddingVelocity = 100;
+
+
+    public static final double[] kPadding = {
+            kShooterPaddingVelocity, kHoodPaddingDegrees};
+
+    //turret
+    public static final double kTurretDOF = 360;
+
+    //hood
+    public static double kDefaultHoodAngle = Math.toRadians(0);
+    public static boolean kUseHoodAutoAimPolynomial = false;
+
+    public static boolean kUseSmartdashboard = false;
+
+    public static InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble> kHoodAutoAimMap = new InterpolatingTreeMap<>();
+    public static PolynomialRegression kHoodAutoAimPolynomial;
+
+    public static double[][] kHoodManualAngle = {
+        /* TEMPLATE REGRESSION */
+        // @x --> distance from target (in meters)
+        // @y --> hood angle (in degrees)
+        { 1.0, 30 },
+        { 1.5, 40 },
+        { 2.0, 50 },
+        { 2.5, 60 },
+        { 3.0, 65 },
+        { 3.5, 70 },
+        { 4.0, 75 },
+        { 4.5, 80 },
+        { 5.0, 82.5 }
+    };
+
+    static {
+        //iterate through the array and place each point into the interpolating tree
+        for (double[] pair : kHoodManualAngle) {
+            kHoodAutoAimMap.put(new InterpolatingDouble(pair[0]), new InterpolatingDouble(pair[1]));
+        }
+        
+        kHoodAutoAimPolynomial = new PolynomialRegression(kHoodManualAngle, 1);
+    }
+    
+    //shooter
+    public static double kDefaultShootingRPM = 2950.0;
+    public static boolean kUseFlywheelAutoAimPolynomial = false;
+
+    public static InterpolatingTreeMap<InterpolatingDouble, InterpolatingDouble> kFlywheelAutoAimMap = new InterpolatingTreeMap<>();
+    public static PolynomialRegression kFlywheelAutoAimPolynomial;
+
+    public static double[][] kFlywheelManualRPM = {
+        /* TEMPLATE REGRESSION */
+        // @x --> distance from target (in meters)
+        // @y --> shooter velocity (in rpm)
+        { 1.0, 100 },
+        { 1.5, 150 },
+        { 2.0, 200 },
+        { 2.5, 250 },
+        { 3.0, 300 },
+        { 3.5, 350 },
+        { 4.0, 400 },
+        { 4.5, 450 },
+        { 5.0, 500 }
+    };
+
+    static {
+        for (double[] pair : kFlywheelManualRPM) {
+            kFlywheelAutoAimMap.put(new InterpolatingDouble(pair[0]), new InterpolatingDouble(pair[1]));
+        }
+
+        kFlywheelAutoAimPolynomial = new PolynomialRegression(kFlywheelManualRPM, 2);
+    }
+
+}
