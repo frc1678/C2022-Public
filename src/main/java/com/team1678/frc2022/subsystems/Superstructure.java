a7a37a573662ae5c36c79f3ebdfa0f3e0f1ed656
apurvagyan <apurvagyan@gmail.com>
2022-01-31 09:37:58 -0800

    move actions and setpoints to an organized periodicIO container

--- src/main/java/com/team1678/frc2022/subsystems/Superstructure.java
+++ src/main/java/com/team1678/frc2022/subsystems/Superstructure.java
@@ -3,6 +3,7 @@ package com.team1678.frc2022.subsystems;
 import com.team1678.frc2022.loops.Loop;
 import com.team1678.frc2022.regressions.ShooterRegression;
 import com.team254.lib.util.InterpolatingDouble;
+import com.team254.lib.util.ReflectingCSVWriter;
 import com.team254.lib.util.Util;
 
 import edu.wpi.first.wpilibj.Timer;
@@ -28,7 +29,7 @@ public class Superstructure extends Subsystem {
         return mInstance;
     };
 
-    /* REQUIRED INSTANCES */
+    /*** REQUIRED INSTANCES ***/
     private final ControlBoard mControlBoard = ControlBoard.getInstance();
     private final Intake mIntake = Intake.getInstance();
     private final Indexer mIndexer = Indexer.getInstance();
@@ -36,20 +37,36 @@ public class Superstructure extends Subsystem {
     private final Hood mHood = Hood.getInstance();
     private final Limelight mLimelight = Limelight.getInstance();
 
-    /* SUPERSTRUCTURE ACTIONS */
-    public boolean NONE = false; // do nothing
-    public boolean INTAKE = false; // run the intake to pick up cargo
-    public boolean OUTTAKE = false; // reverse the intake to spit out cargo
-    public boolean PREP = false; // spin up and aim with shooting setpoints
-    public boolean SHOOT = false; // shoot cargo
-    public boolean FENDOR = false; // shoot cargo from up against the hub
+    // PeriodicIO instance and paired csv writer
+    public PeriodicIO mPeriodicIO = new PeriodicIO();
+    private ReflectingCSVWriter<PeriodicIO> mCSVWriter = null;
+    
+    /*** CONTAINER FOR SUPERSTRUCTURE ACTIONS AND GOALS ***/
+    public static class PeriodicIO {
+        // INPUTS
+        // (superstructure actions)
+        public boolean NONE = false; // do nothing
+        public boolean INTAKE = false; // run the intake to pick up cargo
+        public boolean OUTTAKE = false; // reverse the intake to spit out cargo
+        public boolean PREP = false; // spin up and aim with shooting setpoints
+        public boolean SHOOT = false; // shoot cargo
+        public boolean FENDOR = false; // shoot cargo from up against the hub
 
-    /* SETPOINT VARIABLES */
+        // looper dt measurement
+        public double dt;
+
+        // OUTPUTS
+        // (superstructure goals/setpoints)
+        Intake.WantedAction real_intake;
+        Indexer.WantedAction real_indexer;
+        double real_shooter;
+        double real_hood;
+        
+    }
+
+    // setpoint tracker variables
     public double mShooterSetpoint = 0.0;
     public double mHoodSetpoint = 10.0; // TODO: arbitrary value, change
-
-    // other status variables
-    double mDt = 0.0;
 
     @Override
     public void registerEnabledLoops(ILooper enabledLooper) {
@@ -69,7 +86,7 @@ public class Superstructure extends Subsystem {
                 outputTelemetry();
 
                 final double end = Timer.getFPGATimestamp();
-                mDt = end - start;
+                mPeriodicIO.dt = end - start;
             }
 
             @Override
@@ -83,47 +100,47 @@ public class Superstructure extends Subsystem {
     public void updateOperatorCommands() {
         // control intake vs. outtake actions
         if (mControlBoard.operator.getTrigger(Side.RIGHT)) {
-            INTAKE = true;
+            mPeriodicIO.INTAKE = true;
         } else if (mControlBoard.operator.getTrigger(Side.LEFT)) {
-            OUTTAKE = true;
+            mPeriodicIO.OUTTAKE = true;
         } else {
-            INTAKE = false;
-            OUTTAKE = false;
+            mPeriodicIO.INTAKE = false;
+            mPeriodicIO.OUTTAKE = false;
         }
 
         // control shooting
         if (mControlBoard.operator.getController().getYButtonPressed()) {
-            SHOOT = !SHOOT;
+            mPeriodicIO.SHOOT = !mPeriodicIO.SHOOT;
         }
 
         // control prepping
         if (mControlBoard.operator.getController().getAButtonPressed()) {
-            PREP = !PREP;
+            mPeriodicIO.PREP = !mPeriodicIO.PREP;
         }
     }
 
     /*** SETTERS FOR SUPERSTRUCTURE ACTIONS OUTSIDE OPERATOR INPUT ***/
     public void setWantIntake(boolean intake) {
-        if (INTAKE != intake) {
-            INTAKE = intake;
+        if (mPeriodicIO.INTAKE != intake) {
+            mPeriodicIO.INTAKE = intake;
         }
     }
     public void setWantOuttake(boolean outtake) {
-        if (OUTTAKE != outtake) {
-            OUTTAKE = outtake;
+        if (mPeriodicIO.OUTTAKE != outtake) {
+            mPeriodicIO.OUTTAKE = outtake;
         }
     }
     public void setWantPrep(boolean wants_prep) {
-        PREP = wants_prep;
+        mPeriodicIO.PREP = wants_prep;
     }
     public void setWantShoot(boolean wants_shoot) {
-        SHOOT = wants_shoot;
+        mPeriodicIO.SHOOT = wants_shoot;
     }
     public void setShooterVelocity(double velocity) {
         mShooterSetpoint = velocity;
     }
     public void setWantIntake() {
-        setWantIntake(!INTAKE);
+        setWantIntake(!mPeriodicIO.INTAKE);
     }
 
     /*** UPDATE SHOOTER AND HOOD GOAL WHEN VISION AIMING ***/
@@ -145,54 +162,48 @@ public class Superstructure extends Subsystem {
      * 
     */
     public void setGoals() {
-        // subsystem wanted actions and setpoints to be set based on superstructure actions
-        Intake.WantedAction real_intake;
-        Indexer.WantedAction real_indexer;
-        double real_shooter;
-        double real_hood;
-
         /* Update subsystem wanted actions and setpoints*/
 
         // update hood setpoint
-        real_hood = mHoodSetpoint;
+        mPeriodicIO.real_hood = mHoodSetpoint;
 
         // update shooter setpoint
-        if (PREP) {
-            real_shooter = mShooterSetpoint;
+        if (mPeriodicIO.PREP) {
+            mPeriodicIO.real_shooter = mShooterSetpoint;
         } else {
-            real_shooter = 0.0;
+            mPeriodicIO.real_shooter = 0.0;
         }
 
         // intake and indexer actions
-        if (SHOOT) {
-            real_intake = Intake.WantedAction.NONE;
+        if (mPeriodicIO.SHOOT) {
+            mPeriodicIO.real_intake = Intake.WantedAction.NONE;
 
             if (isSpunUp()) {
-                real_indexer = Indexer.WantedAction.FEED;
+                mPeriodicIO.real_indexer = Indexer.WantedAction.FEED;
             } else {
-                real_indexer = Indexer.WantedAction.INDEX;
+                mPeriodicIO.real_indexer = Indexer.WantedAction.INDEX;
             }
         } else {
-            if (INTAKE) {
-                real_intake = Intake.WantedAction.INTAKE;
-                real_indexer = Indexer.WantedAction.INDEX;
-            } else if (OUTTAKE) {
-                real_intake = Intake.WantedAction.REVERSE;
-                real_indexer = Indexer.WantedAction.REVERSE;
+            if (mPeriodicIO.INTAKE) {
+                mPeriodicIO.real_intake = Intake.WantedAction.INTAKE;
+                mPeriodicIO.real_indexer = Indexer.WantedAction.INDEX;
+            } else if (mPeriodicIO.OUTTAKE) {
+                mPeriodicIO.real_intake = Intake.WantedAction.REVERSE;
+                mPeriodicIO.real_indexer = Indexer.WantedAction.REVERSE;
             } else {
-                real_intake = Intake.WantedAction.NONE;
-                real_indexer = Indexer.WantedAction.INDEX;
+                mPeriodicIO.real_intake = Intake.WantedAction.NONE;
+                mPeriodicIO.real_indexer = Indexer.WantedAction.INDEX;
             }
         }
 
         /* Set subsystem states + setpoints based on wanted actions */
 
         // set intake and indexer states
-        mIntake.setState(real_intake);
-        mIndexer.setState(real_indexer);
+        mIntake.setState(mPeriodicIO.real_intake);
+        mIndexer.setState(mPeriodicIO.real_indexer);
 
         // set shooter subsystem setpoint
-        if (Math.abs(real_shooter) < Util.kEpsilon) {
+        if (Math.abs(mPeriodicIO.real_shooter) < Util.kEpsilon) {
             mShooter.setOpenLoop(0); // open loop if rpm goal is 0, to smooth spin down and stop belt skipping
         } else {
             mShooter.setVelocity(mShooterSetpoint);
@@ -200,10 +211,10 @@ public class Superstructure extends Subsystem {
 
         // set hood subsystem setpoint
         // clamp the hood goal between min and max hard stops for hood angle
-        real_hood = Util.clamp(real_hood,
+        mPeriodicIO.real_hood = Util.clamp(mPeriodicIO.real_hood,
                 Constants.HoodConstants.kHoodServoConstants.kMinUnitsLimit,
                 Constants.HoodConstants.kHoodServoConstants.kMaxUnitsLimit); 
-        mHood.setSetpointMotionMagic(real_hood);
+        mHood.setSetpointMotionMagic(mPeriodicIO.real_hood);
     }
 
     /*** GET SHOOTER AND HOOD SETPOINTS FROM SUPERSTRUCTURE CONSTANTS REGRESSION ***/
@@ -228,7 +239,6 @@ public class Superstructure extends Subsystem {
         }
     }
 
-    
     public boolean isSpunUp() {
         return mShooter.spunUp();
     }
@@ -247,13 +257,14 @@ public class Superstructure extends Subsystem {
     /* Output superstructure actions and other related statuses */
     public void outputTelemetry() {
         // superstructure action statuses
-        SmartDashboard.putBoolean("Intaking", INTAKE);
-        SmartDashboard.putBoolean("Outtaking", OUTTAKE);
-        SmartDashboard.putBoolean("Prepping", PREP);
-        SmartDashboard.putBoolean("Shooting", SHOOT);
-        SmartDashboard.putBoolean("Fendor Shooting", FENDOR);
+        SmartDashboard.putBoolean("Intaking", mPeriodicIO.INTAKE);
+        SmartDashboard.putBoolean("Outtaking", mPeriodicIO.OUTTAKE);
+        SmartDashboard.putBoolean("Prepping", mPeriodicIO.PREP);
+        SmartDashboard.putBoolean("Shooting", mPeriodicIO.SHOOT);
+        SmartDashboard.putBoolean("Fendor Shooting", mPeriodicIO.FENDOR);
 
         // other status variables
+        SmartDashboard.putNumber("Superstructure dt", mPeriodicIO.dt);
         SmartDashboard.putBoolean("Is Spun Up", isSpunUp());
         SmartDashboard.putBoolean("Has Vision Target", mLimelight.hasTarget());
         SmartDashboard.putBoolean("Is Vision Aimed", mLimelight.isAimed());
