84c973205a5a9994b67891c18c8e080f8c73bbb3
apurvagyan <apurvagyan@gmail.com>
2022-03-06 12:38:43 -0800

    wait 0.5 s for wheels to orient in 2-by-2

--- src/main/java/com/team1678/frc2022/auto/modes/TwobyTwoMode.java
+++ src/main/java/com/team1678/frc2022/auto/modes/TwobyTwoMode.java
@@ -99,6 +99,9 @@ public class TwobyTwoMode extends AutoModeBase {
     // start intaking
     runAction(new LambdaAction(() -> mSuperstructure.setWantIntake(true)));
 
+    // wait for modules to align
+    runAction(new WaitAction(0.5));
+
     // drive to intake our second alliance cargo
     runAction(driveToIntakeSecondShotCargo);
 
