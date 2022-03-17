5f56b79454cb90bf0430ab894ef15e3235ad4a56
Olivia Sagayaradj <oliviasagayaradj@Olivias-MacBook-Pro.local>
2022-03-16 20:08:30 -0700

    delete extra timestamp in superstructure logs

--- src/main/java/com/team1678/frc2022/subsystems/Superstructure.java
+++ src/main/java/com/team1678/frc2022/subsystems/Superstructure.java
@@ -897,7 +897,6 @@ public class Superstructure extends Subsystem {
     public void SendLog() {
         ArrayList<Number> items = new ArrayList<Number>();
         items.add(Timer.getFPGATimestamp());
-        items.add(mPeriodicIO.timestamp);
         items.add(mPeriodicIO.dt);
         items.add(mPeriodicIO.SPIT ? 1.0 : 0.0);
         items.add(mPeriodicIO.REJECT ? 1.0 : 0.0);
