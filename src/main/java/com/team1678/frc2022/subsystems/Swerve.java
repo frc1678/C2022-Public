19b8f4b58c597039727a660b3974404e6d8086eb
Olivia Sagayaradj <oliviasagayaradj@Olivias-MacBook-Pro.local>
2022-03-31 19:01:19 -0700

    fix swerve logs

--- src/main/java/com/team1678/frc2022/subsystems/Swerve.java
+++ src/main/java/com/team1678/frc2022/subsystems/Swerve.java
@@ -132,7 +132,6 @@ public class Swerve extends Subsystem {
             SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Integrated", mod.getState().angle.getDegrees());
             SmartDashboard.putNumber("Mod " + mod.moduleNumber + " Velocity", mod.getState().speedMetersPerSecond);    
         }
-
         SmartDashboard.putBoolean("Wants Auto Vision Aim", mWantsAutoVisionAim);
         SmartDashboard.putNumber("Vision Align Target Angle", Math.toDegrees(mLimelightVisionAlignGoal));
         SmartDashboard.putNumber("Swerve Heading", MathUtil.inputModulus(getYaw().getDegrees(), 0, 360));
@@ -356,6 +355,8 @@ public class Swerve extends Subsystem {
         mPeriodicIO.robot_pitch = getPitch().getDegrees();
         mPeriodicIO.robot_roll = getRoll().getDegrees();
         mPeriodicIO.snap_target = Math.toDegrees(snapPIDController.getGoal().position);
+        mPeriodicIO.vision_align_target_angle = Math.toDegrees(mLimelightVisionAlignGoal);
+        mPeriodicIO.swerve_heading = MathUtil.inputModulus(getYaw().getDegrees(), 0, 360);
 
         mPeriodicIO.angular_velocity = chassisVelocity.omegaRadiansPerSecond;
 
@@ -371,6 +372,8 @@ public class Swerve extends Subsystem {
         public double robot_pitch;
         public double robot_roll;
         public double snap_target;
+        public double vision_align_target_angle;
+        public double swerve_heading;
 
         public double angular_velocity;
         public double goal_velocity;
@@ -400,6 +403,14 @@ public class Swerve extends Subsystem {
         headers.add("robot_pitch");
         headers.add("robot_roll");
         headers.add("snap_target");
+        headers.add("vision_align_target_angle");
+        headers.add("swerve_heading");
+        for (SwerveModule module : this.mSwerveMods) {
+            headers.add(module.moduleNumber + "_angle");
+            headers.add(module.moduleNumber + "_desired_angle");
+            headers.add(module.moduleNumber + "_velocity");
+            headers.add(module.moduleNumber + "_cancoder");
+        }
 
         headers.add("angular_velocity");
         headers.add("goal_velocity");
@@ -419,6 +430,14 @@ public class Swerve extends Subsystem {
         items.add(mPeriodicIO.robot_pitch);
         items.add(mPeriodicIO.robot_roll);
         items.add(mPeriodicIO.snap_target);
+        items.add(mPeriodicIO.vision_align_target_angle);
+        items.add(mPeriodicIO.swerve_heading);
+        for (SwerveModule module : this.mSwerveMods) {
+            items.add(module.getState().angle.getDegrees());
+            items.add(module.getTargetAngle());
+            items.add(module.getState().speedMetersPerSecond);
+            items.add(MathUtil.inputModulus(module.getCanCoder().getDegrees() - module.angleOffset, 0, 360));
+        }
 
         items.add(mPeriodicIO.angular_velocity);
         items.add(mPeriodicIO.goal_velocity);
@@ -430,3 +449,4 @@ public class Swerve extends Subsystem {
     }
 
 }
+
