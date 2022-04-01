1a1b4eea6909108eb318623886a015fe26a2ffea
Olivia Sagayaradj <oliviasagayaradj@Olivias-MacBook-Pro.local>
2022-03-31 18:57:07 -0700

    align superstructure logs

--- src/main/java/com/team1678/frc2022/subsystems/Superstructure.java
+++ src/main/java/com/team1678/frc2022/subsystems/Superstructure.java
@@ -1010,18 +1010,17 @@ public class Superstructure extends Subsystem {
 
     public void SendLog() {
         ArrayList<Number> items = new ArrayList<Number>();
-        items.add(Timer.getFPGATimestamp());
         items.add(mPeriodicIO.timestamp);
         items.add(mPeriodicIO.dt);
-        items.add(mPeriodicIO.SPIT ? 1.0 : 0.0);
+        items.add(mPeriodicIO.INTAKE ? 1.0 : 0.0);
+        items.add(mPeriodicIO.REVERSE ? 1.0 : 0.0);
         items.add(mPeriodicIO.REJECT ? 1.0 : 0.0);
         items.add(mPeriodicIO.FORCE_HOLD ? 1.0 : 0.0);
         items.add(mPeriodicIO.EJECT ? 1.0 : 0.0);
-        items.add(mPeriodicIO.REVERSE ? 1.0 : 0.0);
         items.add(mPeriodicIO.PREP ? 1.0 : 0.0);
         items.add(mPeriodicIO.SHOOT ? 1.0 : 0.0);
-        items.add(mPeriodicIO.INTAKE ? 1.0 : 0.0);
         items.add(mPeriodicIO.FENDER ? 1.0 : 0.0);
+        items.add(mPeriodicIO.SPIT ? 1.0 : 0.0);
         items.add(mPeriodicIO.real_shooter);
         items.add(mPeriodicIO.real_hood);
         items.add(mSwerve.getRoll().getDegrees());
